d5585ac3af982188f7f619e5b7e2e1ebba221602
p123-stack <pratikshazalte83@gmail.com>
2025-01-06 18:01:27 +0530

    Profilequery Plan test

--- src/Results/ResultSet.php
+++ src/Results/ResultSet.php
@@ -4,6 +4,8 @@ namespace Neo4j\QueryAPI\Results;
 use ArrayIterator;
 use Countable;
 use IteratorAggregate;
+use Neo4j\QueryAPI\Objects\ProfiledQueryPlan;
+use Neo4j\QueryAPI\Objects\QueryArguments;
 use Neo4j\QueryAPI\Objects\ResultCounters;
 use Traversable;
 
@@ -12,7 +14,7 @@ class ResultSet implements IteratorAggregate, Countable
     /**
      * @param list<ResultRow> $rows
      */
-    public function __construct(private readonly array $rows,private ResultCounters $counters)
+    public function __construct(private readonly array $rows, private ResultCounters $counters, private ?ProfiledQueryPlan $profiledQueryPlan = null , private ?QueryArguments $queryArguments = null)
     {
     }
 
@@ -20,15 +22,25 @@ class ResultSet implements IteratorAggregate, Countable
     {
         return new ArrayIterator($this->rows);
     }
+
     public function getQueryCounters(): ?ResultCounters
     {
         return $this->counters;
+    }
+
+    public function getProfiledQueryPlan(): ?ProfiledQueryPlan
+    {
+        return $this->profiledQueryPlan;
+    }
+
+    public function getQueryArguments(): ?QueryArguments
+    {
+        return $this->queryArguments;
     }
 
     public function count(): int
     {
         return count($this->rows);
     }
-
-
 }
+
