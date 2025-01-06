6c659e655da5caa241aeb429df212453369b201a
p123-stack <pratikshazalte83@gmail.com>
2025-01-06 12:17:15 +0530

    temp

--- src/Query_Profile_run.php
+++ src/Query_Profile_run.php
@@ -10,9 +10,13 @@ $password = '***REMOVED***';
 
 $client = new Profile($neo4jUrl, $username, $password);
 
-$query = "PROFILE MATCH (n:Person) RETURN n";
-$data = $client->executeQuery($query);
+$params = ['name' => 'Alice'];
+
+$query = "PROFILE MATCH (n:Person {name: \$name}) RETURN n.name";
+
+$data = $client->executeQuery($query, $params);
+
+
 $formattedResponse = $client->formatResponse($data);
 
 echo json_encode($formattedResponse, JSON_PRETTY_PRINT);
-
