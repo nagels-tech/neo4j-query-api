ed04c69380f1e8a8e7e6ecc28dacea446ec4a205
Pratiksha <pratiksha@Pratiksha-Nagels>
2025-02-10 14:47:11 +0530

    winp

--- tests/Unit/Neo4jQueryAPIUnitTest.php
+++ tests/Unit/Neo4jQueryAPIUnitTest.php
@@ -6,6 +6,7 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
 use Neo4j\QueryAPI\Neo4jQueryAPI;
 use Neo4j\QueryAPI\Objects\Authentication;
@@ -13,13 +14,12 @@ use Neo4j\QueryAPI\Objects\Bookmarks;
 use Neo4j\QueryAPI\Objects\ResultCounters;
 use Neo4j\QueryAPI\Objects\ResultSet;
 use Neo4j\QueryAPI\Results\ResultRow;
+use Neo4j\QueryAPI\AuthenticateInterface;
 use PHPUnit\Framework\TestCase;
 
 class Neo4jQueryAPIUnitTest extends TestCase
 {
     protected string $address;
-    protected string $username;
-    protected string $password;
 
     protected function setUp(): void
     {
@@ -31,27 +31,39 @@ class Neo4jQueryAPIUnitTest extends TestCase
     {
         return Neo4jQueryAPI::login(
             $this->address,
-            Authentication::basic("neo4j", "9lWmptqBgxBOz8NVcTJjgs3cHPyYmsy63ui6Spmw1d0")
+            Authentication::fromEnvironment()
         );
     }
 
     public function testCorrectClientSetup(): void
     {
+
         $neo4jQueryAPI = $this->initializeApi();
 
         $clientReflection = new \ReflectionClass(Neo4jQueryAPI::class);
+
+
         $clientProperty = $clientReflection->getProperty('client');
         $client = $clientProperty->getValue($neo4jQueryAPI);
-
         $this->assertInstanceOf(Client::class, $client);
 
-        $config = $client->getConfig();
-        $expectedAuthHeader = 'Basic ' . base64_encode('neo4j:9lWmptqBgxBOz8NVcTJjgs3cHPyYmsy63ui6Spmw1d0');
+        $authProperty = $clientReflection->getProperty('auth');
+        $auth = $authProperty->getValue($neo4jQueryAPI);
+        $this->assertInstanceOf(AuthenticateInterface::class, $auth);
 
-        //  $this->assertEquals(rtrim($this->address, '/'), $config['base_uri']);
-        //  $this->assertArrayHasKey('Authorization', $config['headers'], 'Authorization header missing.');
-        //  $this->assertEquals($expectedAuthHeader, $config['headers']['Authorization'], 'Authorization header value mismatch.');
-        $this->assertEquals('application/vnd.neo4j.query', $config['headers']['Content-Type']);
+
+        $expectedAuth = Authentication::fromEnvironment();
+        $this->assertEquals($expectedAuth->getHeader(), $auth->getHeader(), 'Authentication headers mismatch');
+
+        $request = new Request('GET', '/test-endpoint');
+        $authenticatedRequest = $auth->authenticate($request);
+
+
+        $expectedAuthHeader = 'Basic ' . base64_encode(getenv("NEO4J_USERNAME") . ':' . getenv("NEO4J_PASSWORD"));
+        $this->assertEquals($expectedAuthHeader, $authenticatedRequest->getHeaderLine('Authorization'));
+
+        $requestWithHeaders = $authenticatedRequest->withHeader('Content-Type', 'application/vnd.neo4j.query');
+        $this->assertEquals('application/vnd.neo4j.query', $requestWithHeaders->getHeaderLine('Content-Type'));
     }
 
     /**
@@ -59,21 +71,25 @@ class Neo4jQueryAPIUnitTest extends TestCase
      */
     public function testRunSuccess(): void
     {
-
         $mock = new MockHandler([
             new Response(200, ['X-Foo' => 'Bar'], '{"data": {"fields": ["hello"], "values": [[{"$type": "String", "_value": "world"}]]}}'),
         ]);
 
-        $auth = Authentication::basic("neo4j", "9lWmptqBgxBOz8NVcTJjgs3cHPyYmsy63ui6Spmw1d0");
-
+        $auth = Authentication::fromEnvironment();
         $handlerStack = HandlerStack::create($mock);
         $client = new Client(['handler' => $handlerStack]);
 
         $neo4jQueryAPI = new Neo4jQueryAPI($client, $auth);
 
         $cypherQuery = 'MATCH (n:Person) RETURN n LIMIT 5';
-
         $result = $neo4jQueryAPI->run($cypherQuery);
-        $this->assertEquals(new ResultSet([new ResultRow(['hello' => 'world'])], new ResultCounters(), new Bookmarks([])), $result);
+
+        $expectedResult = new ResultSet(
+            [new ResultRow(['hello' => 'world'])],
+            new ResultCounters(),
+            new Bookmarks([])
+        );
+
+        $this->assertEquals($expectedResult, $result);
     }
 }
