bf59aa9ab332393bfc2e69fc64b4e0aa92ea8942
Pratiksha <pratiksha@Pratiksha-Nagels>
2025-02-17 18:09:52 +0530

    winp

---
+++ tests/Integration/Neo4jQueryAPITest.php
@@ -0,0 +1,44 @@
+<?php
+namespace Neo4j\QueryAPI\Tests\Integration;
+
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+use Neo4j\QueryAPI\Neo4jQueryAPI;
+use Neo4j\QueryAPI\Objects\Authentication;
+use Neo4j\QueryAPI\Configuration;
+
+class Neo4jQueryAPITest extends TestCase
+{
+    public function testLoginWithConfigurationWithoutAddress()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage("Address (baseUri) must be provided.");
+        $config = new Configuration(baseUri: "");
+
+        Neo4jQueryAPI::login('http://myaddress', Authentication::fromEnvironment(), $config);
+    }
+
+    public function testLoginWithValidConfiguration()
+    {
+        $config = new Configuration(baseUri: 'http://valid.address');
+
+        $api = Neo4jQueryAPI::login('http://myaddress', Authentication::fromEnvironment(), $config);
+
+        $this->assertInstanceOf(Neo4jQueryAPI::class, $api);
+        $this->assertEquals('http://valid.address', $config->baseUri);
+    }
+    public function testLoginWithEmptyAddress()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage("Address (baseUri) must be provided.");
+
+        $config = new Configuration(baseUri: "");
+
+        Neo4jQueryAPI::login('http://myaddress', Authentication::fromEnvironment(), $config);
+    }
+    /**
+     * Test to ensure that an exception is thrown when baseUri is empty.
+     */
+
+
+}
