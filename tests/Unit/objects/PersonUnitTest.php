2941b51262d09aac4f0cd34d1cfd9f30bc7a328e
Pratiksha <pratiksha@Pratiksha-Nagels>
2025-02-24 18:06:37 +0530

    fixed the CI and Psalm show-infor errors

--- tests/Unit/objects/PersonUnitTest.php
+++ tests/Unit/objects/PersonUnitTest.php
@@ -5,6 +5,6 @@ namespace Neo4j\QueryAPI\Tests\Unit\objects;
 use Neo4j\QueryAPI\Objects\Person;
 use PHPUnit\Framework\TestCase;
 
-class PersonUnitTest extends TestCase
+final class PersonUnitTest extends TestCase
 {
 }
