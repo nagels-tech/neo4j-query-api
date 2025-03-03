9e791b95540126dea89632660bc301dcee7c313b
pratikshazalte69 <pratikshazalte69@gmail.com>
2025-03-03 12:47:59 +0530

    Deprecations fixed

--- tests/Integration/DataTypesIntegrationTest.php
+++ tests/Integration/DataTypesIntegrationTest.php
@@ -33,10 +33,10 @@ final class DataTypesIntegrationTest extends TestCase
                 new ResultRow(['n.name' => 'bob1']),
                 new ResultRow(['n.name' => 'alicy']),
             ],
-            new ResultCounters(),
             new Bookmarks([]),
-            null,
-            AccessMode::WRITE
+            AccessMode::WRITE,
+            new ResultCounters(),
+            null
         );
 
         $results = $this->api->run('MATCH (n:Person) WHERE n.name IN $names RETURN n.name', [
@@ -53,10 +53,10 @@ final class DataTypesIntegrationTest extends TestCase
             [
                 new ResultRow(['n.name' => 'bob1']),
             ],
-            new ResultCounters(),
             new Bookmarks([]),
-            null,
-            AccessMode::WRITE
+            AccessMode::WRITE,
+            new ResultCounters(),
+            null
         );
 
         $results = $this->api->run('MATCH (n:Person) WHERE n.name = $name RETURN n.name LIMIT 1', [
@@ -75,15 +75,15 @@ final class DataTypesIntegrationTest extends TestCase
             [
                 new ResultRow(['n.age' => 30]),
             ],
+            new Bookmarks([]),
+            AccessMode::WRITE,
             new ResultCounters(
                 containsUpdates: true,
                 nodesCreated: 1,
                 propertiesSet: 1,
                 labelsAdded: 1,
             ),
-            new Bookmarks([]),
-            null,
-            AccessMode::WRITE
+            null
         );
 
         $results = $this->api->run('CREATE (n:Person {age: $age}) RETURN n.age', [
@@ -103,15 +103,15 @@ final class DataTypesIntegrationTest extends TestCase
             [
                 new ResultRow(['n.height' => 1.75]),
             ],
+            new Bookmarks([]),
+            AccessMode::WRITE,
             new ResultCounters(
                 containsUpdates: true,
                 nodesCreated: 1,
                 propertiesSet: 1,
                 labelsAdded: 1,
             ),
-            new Bookmarks([]),
-            null,
-            AccessMode::WRITE
+            null
         );
 
         $results = $this->api->run('CREATE (n:Person {height: $height}) RETURN n.height', [
@@ -124,21 +124,22 @@ final class DataTypesIntegrationTest extends TestCase
         $this->assertCount(1, $bookmarks);
     }
 
+
     public function testWithNull(): void
     {
         $expected = new ResultSet(
             [
                 new ResultRow(['n.middleName' => null]),
             ],
+            new Bookmarks([]),
+            AccessMode::WRITE,
             new ResultCounters(
                 containsUpdates: true,
                 nodesCreated: 1,
                 propertiesSet: 0,
                 labelsAdded: 1,
             ),
-            new Bookmarks([]),
-            null,
-            AccessMode::WRITE
+            null
         );
 
         $results = $this->api->run('CREATE (n:Person {middleName: $middleName}) RETURN n.middleName', [
@@ -151,21 +152,22 @@ final class DataTypesIntegrationTest extends TestCase
         $this->assertCount(1, $bookmarks);
     }
 
+
     public function testWithBoolean(): void
     {
         $expected = new ResultSet(
             [
                 new ResultRow(['n.isActive' => true]),
             ],
+            new Bookmarks([]),
+            AccessMode::WRITE,
             new ResultCounters(
                 containsUpdates: true,
                 nodesCreated: 1,
                 propertiesSet: 1,
                 labelsAdded: 1,
             ),
-            new Bookmarks([]),
-            null,
-            AccessMode::WRITE
+            null
         );
 
         $results = $this->api->run('CREATE (n:Person {isActive: $isActive}) RETURN n.isActive', [
@@ -178,21 +180,22 @@ final class DataTypesIntegrationTest extends TestCase
         $this->assertCount(1, $bookmarks);
     }
 
+
     public function testWithString(): void
     {
         $expected = new ResultSet(
             [
                 new ResultRow(['n.name' => 'Alice']),
             ],
+            new Bookmarks([]),
+            AccessMode::WRITE,
             new ResultCounters(
                 containsUpdates: true,
                 nodesCreated: 1,
                 propertiesSet: 1,
                 labelsAdded: 1,
             ),
-            new Bookmarks([]),
-            null,
-            AccessMode::WRITE
+            null
         );
 
         $results = $this->api->run('CREATE (n:Person {name: $name}) RETURN n.name', [
@@ -205,6 +208,7 @@ final class DataTypesIntegrationTest extends TestCase
         $this->assertCount(1, $bookmarks);
     }
 
+
     public function testWithArray(): void
     {
         $expected = new ResultSet(
@@ -212,15 +216,15 @@ final class DataTypesIntegrationTest extends TestCase
                 new ResultRow(['n.name' => 'bob1']),
                 new ResultRow(['n.name' => 'alicy'])
             ],
+            new Bookmarks([]),
+            AccessMode::WRITE,
             new ResultCounters(
                 containsUpdates: false,
                 nodesCreated: 0,
                 propertiesSet: 0,
                 labelsAdded: 0,
             ),
-            new Bookmarks([]),
-            null,
-            AccessMode::WRITE
+            null
         );
 
         $results = $this->api->run(
@@ -233,22 +237,22 @@ final class DataTypesIntegrationTest extends TestCase
         $this->assertCount(1, $bookmarks);
     }
 
+
     public function testWithDate(): void
     {
         $expected = new ResultSet(
             [
                 new ResultRow(['n.date' => '2024-12-11T11:00:00Z'])
-
             ],
+            new Bookmarks([]),
+            AccessMode::WRITE,
             new ResultCounters(
                 containsUpdates: true,
                 nodesCreated: 1,
                 propertiesSet: 1,
-                labelsAdded: 1,
+                labelsAdded: 1
             ),
-            new Bookmarks([]),
-            null,
-            AccessMode::WRITE
+            null
         );
 
         $results = $this->api->run(
@@ -266,23 +270,22 @@ final class DataTypesIntegrationTest extends TestCase
     {
         $expected = new ResultSet(
             [
-                new ResultRow(['n.duration' => 'P14DT16H12M']),
-
+                new ResultRow(['n.duration' => 'P14DT16H12M'])
             ],
+            new Bookmarks([]),
+            AccessMode::WRITE,
             new ResultCounters(
                 containsUpdates: true,
                 nodesCreated: 1,
                 propertiesSet: 1,
-                labelsAdded: 1,
+                labelsAdded: 1
             ),
-            new Bookmarks([]),
-            null,
-            AccessMode::WRITE
+            null
         );
 
         $results = $this->api->run(
             'CREATE (n:Person {duration: duration($duration)}) RETURN n.duration',
-            ['duration' => 'P14DT16H12M'],
+            ['duration' => 'P14DT16H12M']
         );
 
         $this->assertEquals($expected->counters, $results->counters);
@@ -295,17 +298,17 @@ final class DataTypesIntegrationTest extends TestCase
     {
         $expected = new ResultSet(
             [
-                new ResultRow(['n.Point' => 'SRID=4326;POINT (1.2 3.4)']),
+                new ResultRow(['n.Point' => 'SRID=4326;POINT (1.2 3.4)'])
             ],
+            new Bookmarks([]),
+            AccessMode::WRITE,
             new ResultCounters(
                 containsUpdates: true,
                 nodesCreated: 1,
                 propertiesSet: 1,
-                labelsAdded: 1,
+                labelsAdded: 1
             ),
-            new Bookmarks([]),
-            null,
-            AccessMode::WRITE
+            null
         );
 
         $results = $this->api->run(
@@ -314,10 +317,10 @@ final class DataTypesIntegrationTest extends TestCase
                 'Point' => [
                     'longitude' => 1.2,
                     'latitude' => 3.4,
-                    'crs' => 'wgs-84',
-                ]]
+                    'crs' => 'wgs-84'
+                ]
+            ]
         );
-
 
         $this->assertEquals($expected->counters, $results->counters);
         $this->assertEquals(iterator_to_array($expected), iterator_to_array($results));
@@ -329,17 +332,17 @@ final class DataTypesIntegrationTest extends TestCase
     {
         $expected = new ResultSet(
             [
-                new ResultRow(['n.Point' => new Point(1.2, 3.4, 4.2, 4979)]),
+                new ResultRow(['n.Point' => new Point(1.2, 3.4, 4.2, 4979)])
             ],
+            new Bookmarks([]),
+            AccessMode::WRITE,
             new ResultCounters(
                 containsUpdates: true,
                 nodesCreated: 1,
                 propertiesSet: 1,
-                labelsAdded: 1,
+                labelsAdded: 1
             ),
-            new Bookmarks([]),
-            null,
-            AccessMode::WRITE
+            null
         );
 
         $results = $this->api->run(
@@ -348,10 +351,9 @@ final class DataTypesIntegrationTest extends TestCase
                 'longitude' => 1.2,
                 'latitude' => 3.4,
                 'height' => 4.2,
-                'srid' => 4979,
+                'srid' => 4979
             ]
         );
-
 
         $this->assertEquals($expected->counters, $results->counters);
         $this->assertEquals(iterator_to_array($expected), iterator_to_array($results));
@@ -363,17 +365,17 @@ final class DataTypesIntegrationTest extends TestCase
     {
         $expected = new ResultSet(
             [
-                new ResultRow(['n.Point' => new Point(10.5, 20.7, null, 7203)]),
+                new ResultRow(['n.Point' => new Point(10.5, 20.7, null, 7203)])
             ],
+            new Bookmarks([]),
+            AccessMode::WRITE,
             new ResultCounters(
                 containsUpdates: true,
                 nodesCreated: 1,
                 propertiesSet: 1,
-                labelsAdded: 1,
+                labelsAdded: 1
             ),
-            new Bookmarks([]),
-            null,
-            AccessMode::WRITE
+            null
         );
 
         $results = $this->api->run(
@@ -381,10 +383,9 @@ final class DataTypesIntegrationTest extends TestCase
             [
                 'x' => 10.5,
                 'y' => 20.7,
-                'srid' => 7203,
+                'srid' => 7203
             ]
         );
-
 
         $this->assertEquals($expected->counters, $results->counters);
         $this->assertEquals(iterator_to_array($expected), iterator_to_array($results));
@@ -396,17 +397,17 @@ final class DataTypesIntegrationTest extends TestCase
     {
         $expected = new ResultSet(
             [
-                new ResultRow(['n.Point' => new Point(10.5, 20.7, 30.9, 9157)]),
+                new ResultRow(['n.Point' => new Point(10.5, 20.7, 30.9, 9157)])
             ],
+            new Bookmarks([]),
+            AccessMode::WRITE,
             new ResultCounters(
                 containsUpdates: true,
                 nodesCreated: 1,
                 propertiesSet: 1,
-                labelsAdded: 1,
+                labelsAdded: 1
             ),
-            new Bookmarks([]),
-            null,
-            AccessMode::WRITE
+            null
         );
 
         $results = $this->api->run(
@@ -415,10 +416,9 @@ final class DataTypesIntegrationTest extends TestCase
                 'x' => 10.5,
                 'y' => 20.7,
                 'z' => 30.9,
-                'srid' => 9157,
+                'srid' => 9157
             ]
         );
-
 
         $this->assertEquals($expected->counters, $results->counters);
         $this->assertEquals(iterator_to_array($expected), iterator_to_array($results));
@@ -440,19 +440,18 @@ final class DataTypesIntegrationTest extends TestCase
                         'labels' => [
                             0 => 'Person'
                         ]
-
                     ]
-                ]),
+                ])
             ],
+            new Bookmarks([]),
+            AccessMode::WRITE,
             new ResultCounters(
                 containsUpdates: true,
                 nodesCreated: 1,
                 propertiesSet: 3,
-                labelsAdded: 1,
+                labelsAdded: 1
             ),
-            new Bookmarks([]),
-            null,
-            AccessMode::WRITE
+            null
         );
 
         $results = $this->api->run(
@@ -460,10 +459,9 @@ final class DataTypesIntegrationTest extends TestCase
             [
                 'name' => 'Ayush',
                 'age' => 30,
-                'location' => 'New York',
+                'location' => 'New York'
             ]
         );
-
 
         $this->assertEquals($expected->counters, $results->counters);
         $this->assertEquals(iterator_to_array($expected), iterator_to_array($results));
@@ -475,45 +473,45 @@ final class DataTypesIntegrationTest extends TestCase
     {
         $expected = new ResultSet(
             [
-                new ResultRow(['node1' => [
-                    'labels' => ['Person'],
-                    'properties' => [
-                        'name' => 'A',
+                new ResultRow([
+                    'node1' => [
+                        'labels' => ['Person'],
+                        'properties' => [
+                            'name' => 'A'
+                        ]
                     ],
-                ],
                     'node2' => [
                         'labels' => ['Person'],
                         'properties' => [
-                            'name' => 'B',
-                        ],
+                            'name' => 'B'
+                        ]
                     ],
-                    'relationshipTypes' => ['FRIENDS'],
-                ]),
+                    'relationshipTypes' => ['FRIENDS']
+                ])
             ],
+            new Bookmarks([]),
+            AccessMode::WRITE,
             new ResultCounters(
                 containsUpdates: true,
                 nodesCreated: 2,
                 propertiesSet: 2,
                 relationshipsCreated: 1,
-                labelsAdded: 2,
+                labelsAdded: 2
             ),
-            new Bookmarks([]),
-            null,
-            AccessMode::WRITE
+            null
         );
 
         $results = $this->api->run(
             'CREATE (a:Person {name: $name1}), (b:Person {name: $name2}),
-     (a)-[r:FRIENDS]->(b)
-     RETURN {labels: labels(a), properties: properties(a)} AS node1,
-            {labels: labels(b), properties: properties(b)} AS node2,
-            collect(type(r)) AS relationshipTypes',
+         (a)-[r:FRIENDS]->(b)
+         RETURN {labels: labels(a), properties: properties(a)} AS node1,
+                {labels: labels(b), properties: properties(b)} AS node2,
+                collect(type(r)) AS relationshipTypes',
             [
                 'name1' => 'A',
-                'name2' => 'B',
+                'name2' => 'B'
             ]
         );
-
 
         $this->assertEquals($expected->counters, $results->counters);
         $this->assertEquals(iterator_to_array($expected), iterator_to_array($results));
@@ -521,32 +519,27 @@ final class DataTypesIntegrationTest extends TestCase
         $this->assertCount(1, $bookmarks);
     }
 
-
     public function testWithMap(): void
     {
         $expected = new ResultSet(
             [
-                new ResultRow(['map' => [
-                    'hello' => 'hello',
-                ],
-                ]),
+                new ResultRow(['map' => ['hello' => 'hello']])
             ],
+            new Bookmarks([]),
+            AccessMode::WRITE,
             new ResultCounters(
                 containsUpdates: false,
                 nodesCreated: 0,
                 propertiesSet: 0,
-                labelsAdded: 0,
+                labelsAdded: 0
             ),
-            new Bookmarks([]),
-            null,
-            AccessMode::WRITE
+            null
         );
 
         $results = $this->api->run(
             'RETURN {hello: "hello"} AS map',
             []
         );
-
 
         $this->assertEquals($expected->counters, $results->counters);
         $this->assertEquals(iterator_to_array($expected), iterator_to_array($results));
@@ -564,39 +557,39 @@ final class DataTypesIntegrationTest extends TestCase
                         'properties' => [
                             'name' => 'Ayush',
                             'age' => 30,
-                            'location' => 'New York',
-                        ],
+                            'location' => 'New York'
+                        ]
                     ],
                     'node2' => [
                         'labels' => ['Person'],
                         'properties' => [
                             'name' => 'John',
                             'age' => 25,
-                            'location' => 'Los Angeles',
-                        ],
+                            'location' => 'Los Angeles'
+                        ]
                     ],
-                    'relationshipType' => 'FRIEND_OF',
-                ]),
+                    'relationshipType' => 'FRIEND_OF'
+                ])
             ],
+            new Bookmarks([]),
+            AccessMode::WRITE,
             new ResultCounters(
                 containsUpdates: true,
                 nodesCreated: 2,
                 propertiesSet: 6,
                 relationshipsCreated: 1,
-                labelsAdded: 2,
+                labelsAdded: 2
             ),
-            new Bookmarks([]),
-            null,
-            AccessMode::WRITE
+            null
         );
 
         $results = $this->api->run(
             'CREATE (p1:Person {name: $name1, age: $age1, location: $location1}),
-             (p2:Person {name: $name2, age: $age2, location: $location2}),
-             (p1)-[r:FRIEND_OF]->(p2)
-     RETURN {labels: labels(p1), properties: properties(p1)} AS node1,
-            {labels: labels(p2), properties: properties(p2)} AS node2,
-           type(r) AS relationshipType',
+         (p2:Person {name: $name2, age: $age2, location: $location2}),
+         (p1)-[r:FRIEND_OF]->(p2)
+         RETURN {labels: labels(p1), properties: properties(p1)} AS node1,
+                {labels: labels(p2), properties: properties(p2)} AS node2,
+                type(r) AS relationshipType',
             [
                 'name1' => 'Ayush',
                 'age1' => 30,
@@ -607,10 +600,10 @@ final class DataTypesIntegrationTest extends TestCase
             ]
         );
 
-
         $this->assertEquals($expected->counters, $results->counters);
         $this->assertEquals(iterator_to_array($expected), iterator_to_array($results));
         $bookmarks = $results->bookmarks;
         $this->assertCount(1, $bookmarks);
     }
+
 }
